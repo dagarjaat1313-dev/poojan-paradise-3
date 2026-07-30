08c8ecf0d8ba2c0605f82cb75e78cc4a8a1cbddf
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 00:05:44 +0530

    Create ProductCard.jsx

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import products from "../../data/products";
+import ProductCard from "../ProductCard/ProductCard";
+import "./FeaturedProducts.css";
+
+export default function FeaturedProducts() {
+  return (
+    <section className="featured">
+      <h2>Best Sellers</h2>
+
+      <div className="products-grid">
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+}
