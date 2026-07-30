87c727407ae8ba5f205e0ff07715e0f88a3164cf
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 00:01:03 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Categories from "./components/Categories/Categories";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Categories />
     </>
   );
 }
