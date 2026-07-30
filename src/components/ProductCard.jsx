8c0e2522bc50e96c548158d2235229d16d4e339c
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 00:25:13 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,26 @@
 import "./ProductCard.css";
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
 
 export default function ProductCard({ product }) {
   return (
     <div className="product-card">
+
+      <span className="badge">Best Seller</span>
+
+      <button className="wishlist">
+        <FaHeart />
+      </button>
+
       <img src={product.image} alt={product.name} />
+
       <h3>{product.name}</h3>
-      <p>₹{product.price}</p>
-      <button>Add to Cart</button>
+
+      <p className="price">₹{product.price}</p>
+
+      <button className="cart-btn">
+        <FaShoppingCart /> Add to Cart
+      </button>
+
     </div>
   );
 }
