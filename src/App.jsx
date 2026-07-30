99e902541bdc001762cd6e4ef167f9f19cb94bff
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 01:29:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import WhatsAppButton from "./components/WhatsAppButton";
 
-import Hero from "./components/Hero";
+import Hero from "./components/hero";
 import Categories from "./components/Categories";
 import FeaturedProducts from "./components/FeaturedProducts";
 
