e28062115ca6249ed5d64a0e06ff713d15e7d969
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 00:32:45 +0530

    Create pages/Shop.jsx

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,17 @@
+import products from "../data/products";
+import ProductCard from "../components/ProductCard/ProductCard";
+import "./Shop.css";
+
+export default function Shop() {
+  return (
+    <section className="shop">
+      <h1>All Products</h1>
+
+      <div className="shop-grid">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+}
