0f9130007bde8bbe623c290126d0f7d0735611e2
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 00:51:29 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import WhatsAppButton from "./components/WhatsAppButton/WhatsAppButton";
+
+import Hero from "./components/Hero/Hero";
+import Categories from "./components/Categories/Categories";
+import FeaturedProducts from "./components/FeaturedProducts/FeaturedProducts";
+
 import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Categories />
+      <FeaturedProducts />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Shop />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+
       <Footer />
       <WhatsAppButton />
     </>
