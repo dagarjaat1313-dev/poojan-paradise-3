ce75f3045836caee3e2b5aab45758a8d23557f54
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-30 23:51:10 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+
 export default function App() {
   return (
-    <div>
-      <h1>Poojan Paradise</h1>
-      <p>Premium Pooja Store Coming Soon...</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
