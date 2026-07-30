63440aa3c58201345e8652cc941709daa9fb9bce
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-31 00:53:02 +0530

    Create WishlistContext.jsx

---
+++ src/context/WishlistContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+const WishlistContext = createContext();
+
+export function WishlistProvider({ children }) {
+  const [wishlist, setWishlist] = useState([]);
+
+  const toggleWishlist = (product) => {
+    const exists = wishlist.find((item) => item.id === product.id);
+
+    if (exists) {
+      setWishlist(wishlist.filter((item) => item.id !== product.id));
+    } else {
+      setWishlist([...wishlist, product]);
+    }
+  };
+
+  return (
+    <WishlistContext.Provider
+      value={{
+        wishlist,
+        toggleWishlist,
+      }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+}
+
+export const useWishlist = () => useContext(WishlistContext);
