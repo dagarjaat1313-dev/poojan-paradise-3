4ffe6e8a02004824db1d017397e292f69debf0ab
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-08-01 00:29:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/hero";
 import Categories from "./components/Categories";
+import FeaturedProducts from "./components/FeaturedProducts";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <Categories />
+      <FeaturedProducts />
     </>
   );
 }
